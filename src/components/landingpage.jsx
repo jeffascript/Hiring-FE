4a7a9051c4cca8bfa902628215cc5d204a286202
Rushita <rushi.devi@gmail.com>
2020-05-28 17:17:42 +0200

    added scss

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -1,1 +1,19 @@
 import React, { Component } from 'react';
+import backgroundImg from "../assets/pics/entrepreneur-working-on-his-macbook-2-picjumbo-com.jpg"
+
+class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="cont">
+            <div id="jumbo1"><img src={backgroundImg} alt="no" ></img></div>
+            <div id="jumbo2"></div>
+            </div>            
+         );
+    }
+}
+ 
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+import LandingPage from './components/LandingPage'
+import '../src/styles/style.scss'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
