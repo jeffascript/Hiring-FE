82f0932db2c07f0203c5b2e2d2c2183096a7fb72
rob <robertahuber88@gmail.com>
2020-05-31 16:43:59 +0200

    Button and shifted_files

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { Layout } from "antd";
-const { Header, Footer, Sider, Content } = Layout;
+import Banner from './Banner/Banner'
 
 class LandingPage extends Component {
   constructor(props) {
@@ -10,20 +9,7 @@ class LandingPage extends Component {
   render() {
     return (
       <div className="cont">
-        <div id="jumbo">
-          <div id="logoRow">
-            <div id="logo">
-              <img
-                src={require("../assets/pics/recruitask.png")}
-                alt="logo"
-              ></img>
-            </div>
-          </div>
-          <div id="mottoRow">
-            <div id="motto">The first task-based hiring platform</div>
-            <div id="mottoCaption">We propose real anonymized tasks of companies from all over the world. Train yourself, nail the challenge and get hired.</div>
-          </div>
-        </div>
+        <Banner/>
         <div id="appGoals">
           <div id="goalTitle">Lorem ipsum dolor sit amet.</div>
           <div id="goalCaption">
