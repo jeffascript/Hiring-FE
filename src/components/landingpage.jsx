8a85b2ae0f2b0af631962b80117df6b8fef0138f
rob <robertahuber88@gmail.com>
2020-05-31 14:54:27 +0200

    jumbotron

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -1,33 +1,40 @@
-import React, { Component } from 'react';
-import backgroundImg from "../assets/pics/entrepreneur-working-on-his-macbook-2-picjumbo-com.jpg";
-import { Layout } from 'antd';
+import React, { Component } from "react";
+import { Layout } from "antd";
 const { Header, Footer, Sider, Content } = Layout;
 
 class LandingPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        return (
-            // <div className="cont">
-            // <div id="jumbo"><img src={backgroundImg} alt="no" ></img></div>
-            // <div id="main">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, sit neque adipisci eos dolorem quis deserunt dolores, hic eligendi dignissimos corporis tempora unde vitae consectetur cum ratione rerum quibusdam quaerat?</div>
-            // </div>    
-
-            <div>
-                <Layout>
-                    <Header>Header</Header>
-                    <Layout>
-                        <Sider>Sider</Sider>
-                        <Content>Content</Content>
-                    </Layout>
-                    <Footer>Footer</Footer>
-                </Layout>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="cont">
+        <div id="jumbo">
+          <div id="logoRow">
+            <div id="logo">
+              <img
+                src={require("../assets/pics/recruitask.png")}
+                alt="logo"
+              ></img>
             </div>
-        );
-    }
+          </div>
+          <div id="mottoRow">
+            <div id="motto">The first task-based hiring platform</div>
+            <div id="mottoCaption">We propose real anonymized tasks of companies from all over the world. Train yourself, nail the challenge and get hired.</div>
+          </div>
+        </div>
+        <div id="appGoals">
+          <div id="goalTitle">Lorem ipsum dolor sit amet.</div>
+          <div id="goalCaption">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Perferendis eaque doloribus suscipit pariatur fugit sapiente
+            nesciunt quisquam est necessitatibus.
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default LandingPage;
