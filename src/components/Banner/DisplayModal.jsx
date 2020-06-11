d2628a3714dc1e93a2b294014d529e106d065e70
bubba <boubasambare@yahoo.it>
2020-06-11 12:51:46 +0200

    fix +++

--- src/components/Banner/DisplayModal.jsx
+++ src/components/Banner/DisplayModal.jsx
@@ -1,36 +1,29 @@
 import React, { useState } from "react";
 import { Modal, Button } from 'antd';
 import { useSelector, useDispatch } from 'react-redux'
-
-import { FaLinkedin,  FaGithub } from 'react-icons/fa';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import recruitLogo from "../../assets/pics/recruitask.png"
-
 export default function DisplayModal(props) {
-
-    const [modalVisible, hide] = useState(false);
-
+    const [modalVisible, SetModalVisible] = useState(false);
     const state = useSelector(state => state)
     const currentUser = useSelector(state => state)
-
-    console.log(currentUser,"user");
-    
-    return (        
-        <div>            
+    let { show, SetVisibility } = props
+    console.log(currentUser, "user");
+    return (
+        <div>
             <Modal
-               
                 title="recruitask" icon={recruitLogo} className="titleName"
                 style={{ textAlign: "center" }}
                 centered
-                visible={props.show}
-                onCancel={() => hide(false)}
+                visible={show}
+                onCancel={SetVisibility}
                 animation={false}
             >
-                <Button id="btnGitHub" 
-                 href={`${process.env.REACT_APP_URL}/api/auth/github`}
-                   ><FaGithub /> Login with GitHub</Button>
-
+                <Button id="btnGitHub"
+                    href={`${process.env.REACT_APP_URL}/api/auth/github`}
+                ><FaGithub /> Login with GitHub</Button>
                 <Button id="btnLinkedIn" href={`${process.env.REACT_APP_URL}/api/auth/linkedin`}
-                    ><FaLinkedin />Login with LinkedIn</Button>
+                ><FaLinkedin />Login with LinkedIn</Button>
                 <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi eius hic impedit vitae, esse provident rerum atque enim eveniet error.
                     </p>
@@ -38,6 +31,3 @@ export default function DisplayModal(props) {
         </div>
     );
 }
-
-
-// export default DisplayModal;
