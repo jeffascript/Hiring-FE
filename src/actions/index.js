cddfc403d17d2dea69e1fdeeb5179c2d0145d9ad
Rushita <rushi.devi@gmail.com>
2020-06-16 10:45:16 +0200

    cardList component added

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,27 +21,4 @@ export const getUserDataByToken = (token) => {
     };
   }
 
-  export const getTasksByToken = (token) => {
-    return async (dispatch, getState) => {
-        const apiResp = await fetch( process.env.REACT_APP_URL+  "/api/task/approvedtasks", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + token
-            },
-        });
-        if (apiResp.ok) {
-            const respJson = await apiResp.json()
-            dispatch({
-                type: "GET_TASKS",
-                payload: respJson
-              });
-        }
-        else {
-            console.log(apiResp)
-            // setError("Some problem with your token!")
-        }      
-    };
-  }
-
   
--- src/components/LandingPage/Welcome.jsx
+++ src/components/LandingPage/Welcome.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { getUserDataByToken, getTasksByToken } from "../../actions/index.js";
+import React, { useEffect } from 'react';
+import { getUserDataByToken } from "../../actions/index.js";
 import { useSelector, useDispatch } from 'react-redux'
 import DisplayCard from '../../components/CardComponent/DisplayCard';
 import Header from "../Header/Header.jsx";
@@ -7,24 +7,20 @@ import Header from "../Header/Header.jsx";
 const Welcome = () => {
 
     const state = useSelector(state => state)
-
     const dispatch = useDispatch();
 
     useEffect(() => {
         const token = localStorage.token
-        console.log(token, "toke")
         dispatch(getUserDataByToken(token));
-        dispatch(getTasksByToken(token));
     }, []);
 
-    console.log(state, "state")
     return (
         <>
             {state.loggedInUser &&
                 <>
-                    <div style={{overflowX:"hidden"}} >
+                    <div style={{ overflowX: "hidden" }} >
                         <Header userName={state.loggedInUser.username} />
-                        <DisplayCard task={state.tasks} />
+                        <DisplayCard />
                     </div>
                 </>
             }
