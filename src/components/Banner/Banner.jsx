1f7566801ef4ee0109ac989d18be0a24ab6c0a84
jeffascript <ofoaro.jeff@gmail.com>
2020-06-29 10:08:03 +0200

    landing page commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Container, Row, Col, Image } from "reactstrap"
+import { Container, Row, Col, Image } from "reactstrap";
 import BlockButton from "../BlockButton/BlockButton";
 
 export default () => {
   let text = "Get Started";
 
   return (
-    <Container fluid  id="jumbo">
+    <Container fluid id="jumbo">
       <Row id="logoRow">
         <div id="logo">
           <img
@@ -16,34 +16,33 @@ export default () => {
         </div>
       </Row>
 
-    
-        {/* <Row sm={12} id="motto">The first task-based hiring platform</Row>
+      {/* <Row sm={12} id="motto">The first task-based hiring platform</Row>
         <Row id="mottoCaption">
           We propose real anonymized tasks of companies from all over the world.
           Train yourself, nail the challenge and get hired.
           </Row> */}
 
-          <Container>
-            <Row>
-              <div className="col-12" id="motto">
-              <Container>
-                <span >The first task-based hiring platform</span>
-              </Container>
-              </div>
-              <div className="col-12" id="mottoCaption">
-              <Container>
-                <p >We propose real anonymized tasks of companies from all over the world.
-          Train yourself, nail the challenge and get hired.</p>
-              </Container>
-              </div>
-            </Row>
-          </Container>
-     
-
-        <Row className="appBtn">
-        <BlockButton text={text} />
+      <Container>
+        <Row>
+          <div className="col-12" id="motto">
+            <Container>
+              <span> The first task-based hiring platform </span>
+            </Container>
+          </div>
+          <div className="col-12" id="mottoCaption">
+            <Container>
+              <p>
+                We propose real anonymized tasks of companies from all over the
+                world. Train yourself, nail the challenge and get hired.
+              </p>
+            </Container>
+          </div>
         </Row>
+      </Container>
+
+      <Row className="appBtn">
+        <BlockButton text={text} />
+      </Row>
     </Container>
   );
 };
-
