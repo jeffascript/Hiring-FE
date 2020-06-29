745c4d4ba573d05e9888e044a33caadf37c3c930
jeffascript <ofoaro.jeff@gmail.com>
2020-06-29 09:27:31 +0200

    img fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import newImg from "../../assets/pics/recruitask.png";
 
 export default () => {
   return (
@@ -6,27 +7,21 @@ export default () => {
       <div className="container-fluid text-center text-md-left">
         <div className="row">
           <div className="col-md-6 my-auto">
-          <img
-            src={require("../../assets/pics/recruitask.png")}
-            alt="logo"
-          ></img>
+            <img src={newImg} alt="logo"></img>
           </div>
           <hr className="clearfix w-100 d-md-none pb-3" />
-         <div className="col-md-6 my-auto" >
-           <div className="row">
-          <div className="col-md-4 mb-md-0 mb-3">
-            <h5 className="text-uppercase">ABOUT US</h5>
-          
-          </div>
-          <div className="col-md-4 mb-md-0 mb-3">
-            <h5 className="text-uppercase">SITEMAP</h5>
-            
-          </div>
-          <div className="col-md-4 mb-md-0 mb-3">
-            <h5 className="text-uppercase">CONTACT US</h5>
-            
-          </div>
-          </div>
+          <div className="col-md-6 my-auto">
+            <div className="row">
+              <div className="col-md-4 mb-md-0 mb-3">
+                <h5 className="text-uppercase">ABOUT US</h5>
+              </div>
+              <div className="col-md-4 mb-md-0 mb-3">
+                <h5 className="text-uppercase">SITEMAP</h5>
+              </div>
+              <div className="col-md-4 mb-md-0 mb-3">
+                <h5 className="text-uppercase">CONTACT US</h5>
+              </div>
+            </div>
           </div>
         </div>
       </div>
